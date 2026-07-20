836b1b22548cda22bc645466c29553341fee73a9
imrancoder786 <laptop14072024@gmail.com>
2026-07-20 22:14:25 +0530

    feat: implement greedy decoding and validation functions in train.py

--- Transformer_from_scratch/train.py
+++ Transformer_from_scratch/train.py
@@ -23,6 +23,79 @@ import warnings
 from config import get_config
 
 
+
+def greedy_decode(model ,  source ,source_mask  ,tokenizer_src , tokenizer_tgt , max_len , device):
+    sos_idex = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idex = tokenizer_tgt.token_to_id('[EOS]')
+
+    #precompute the encoder output and it for every token we get from the decoder
+    encoder_output = model.encode(source ,source_mask)
+    # Initialize the deocoder input with the sos token
+    decoder_input = torch.empty(1,1).fill_(sos_idex).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+
+        # build mask for the target (decoder input)
+        decoder_mask = casual_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+
+        #calculate the output of the decoder 
+        out = model.decode(encoder_output , source_mask , decoder_input , decoder_mask)
+
+        #get the next token
+        prob = model.project(out[: ,-1])
+        #select the token with the max probability ( because it is greedy search)
+        _, next_word = torch.max(prob , dim=1)
+        decoder_input = torch.cat([decoder_input , torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)] , dim=1)
+        if next_word == eos_idex:
+            break
+    return decoder_input.squeeze(0)
+
+def run_validation(model ,validation_ds , tokenizer_src ,tokenizer_tgt ,max_len ,device , print_msg ,globel_state ,writer , num_example = 2):
+    model.eval()
+    count = 0
+
+    source_text = []
+    expected = []
+    predicted = []
+
+    #size of the control window (just use a default value)
+    control = 80
+
+    with torch.no_grad():
+        for batch in validation_ds:
+            count += 1
+            encoder_input = batch['encoder_input'].to(device)
+            encoder_mask = batch['encoder_mask'].to(device)
+
+            assert encoder_input.size(0) == 1 , "batch size must be 1 for validation"
+
+            model_out = greedy_decode(model , encoder_input , encoder_mask , tokenizer_src ,tokenizer_tgt, max_len , device)
+
+            source_text = batch['src_text'][0]
+            target_text = batch['tgt_text'][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_text.append(source_text)
+            expected.append(target_text)
+            predicted.append(model_out_text)
+
+            #print it on the console
+            print_msg('-' * console_width)
+            print_msg(f'SOURCE:{source_text}')
+            print_msg(f'TARGET:{target_text}')
+            print_msg(f'PREDICTED:{model_out_text}')
+
+            if count == num_example:
+                break
+            
+
+
+
+
+        
+
+
 def get_all_sentences(ds ,lang):
     for item in ds:
         yield item[lang]
